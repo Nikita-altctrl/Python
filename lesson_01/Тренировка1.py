efaae533ad3511826a6c31759f0da57d9ca08923
Nikita-altctrl <g12nek@gmail.com>
2026-03-30 20:27:10 +0500

    test

---
+++ lesson_01/Тренировка1.py
@@ -0,0 +1,25 @@
+my_heght = 180
+print(my_heght)
+
+my_name = "Никита"
+my_name = "Никита Набоко"
+print(my_name)
+
+pet_name = input("Как зовут вашего питомца? ")
+print("Ваш любимчик - " + pet_name)
+
+def print_python():
+    print("Учу Python!")
+
+print_python()
+
+def print_letter(let):
+    print(let, end="")
+
+print_letter("С")
+print_letter("т")
+print_letter("у")
+print_letter("д")
+print_letter("е")
+print_letter("н")
+print_letter("т")
